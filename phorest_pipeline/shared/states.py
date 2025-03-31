48177afa533979df47edb261dd1ce019ea8b9ada
cr806 <christopher.reardon@york.ac.uk>
2025-03-31 16:31:39 +0100

    Added FATAL_ERROR state to collector so that camera retries can be tracked

---
+++ phorest_pipeline/shared/states.py
@@ -0,0 +1,21 @@
+# src/process_pipeline/shared/states.py
+from enum import Enum, auto
+
+
+class CollectorState(Enum):
+    IDLE = auto()
+    WAITING_TO_RUN = auto()
+    COLLECTING = auto()
+    FATAL_ERROR = auto()
+
+
+class ProcessorState(Enum):
+    IDLE = auto()
+    WAITING_FOR_DATA = auto()
+    PROCESSING = auto()
+
+
+class CommunicatorState(Enum):
+    IDLE = auto()
+    WAITING_FOR_RESULTS = auto()
+    COMMUNICATING = auto()
