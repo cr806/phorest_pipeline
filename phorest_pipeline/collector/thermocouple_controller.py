ee5536f99bde0a656f62e3c246ff552dfb7e2095
cr806 <christopher.reardon@york.ac.uk>
2025-05-06 13:25:17 +0100

    Start to implement true temperature sensor code

--- phorest_pipeline/collector/thermocouple_controller.py
+++ phorest_pipeline/collector/thermocouple_controller.py
@@ -3,6 +3,46 @@ import datetime
 import time
 from pathlib import Path
 
+DEVICE_LOC = Path('/sys/bus/w1/devices/')
+DEVICE_DICT= {
+    '28-00000ff8fa16': 'Sensor 1',
+    '28-00000ff82fc2': 'Sensor 2',
+}
+
+def start_w1():
+    pass
+
+
+def check_device_connection(device_dict) -> bool:
+    for sensor_id in device_dict:
+        if not Path(DEVICE_LOC, sensor_id).exists():
+            print(f'[THERMOCOUPLE] [ERROR] Device {sensor_id} not found at {DEVICE_LOC}.')
+            return False
+        else:
+            print(f'[THERMOCOUPLE] Device {sensor_id} found at {DEVICE_LOC}.')
+
+
+def read_sensor_ROM(device_id):
+    with open(Path(DEVICE_LOC, device_id, 'w1_slave'), 'r') as f:
+        return f.readlines()
+
+
+def read_temp(device_id) -> tuple[int, str, float | None]:
+    lines = read_sensor_ROM(device_id)
+    error_count = 0
+    while lines[0].strip()[-3:] != 'YES' and error_count < 5:
+        time.sleep(0.2)
+        lines = read_sensor_ROM(device_id)
+        error_count += 1
+    if error_count >= 5:
+        return None
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos+2:]
+        return float(temp_string) / 1000.0
+    else:
+        return None
+
 
 def thermocouple_controller(data_dir: Path) -> tuple[int, str, dict | None]:
     """
@@ -15,6 +55,10 @@ def thermocouple_controller(data_dir: Path) -> tuple[int, str, dict | None]:
     metadata_dict = None
 
     try:
+        print('[THERMOCOUPLE] Checking sensor connections ...')
+        if not check_device_connection(DEVICE_DICT):
+            return (1, '[THERMOCOUPLE] [ERROR] Sensor(s) not found.', None)
+
         print('[THERMOCOUPLE] Taking temperature measurements ...')
         time.sleep(0.5)
 
