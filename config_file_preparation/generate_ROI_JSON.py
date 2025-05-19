5317446290777dda70493f08667b02eb0a96e49a
cr806 <christopher.reardon@york.ac.uk>
2025-05-19 11:56:26 +0100

    Added ROI_manifest generator script

---
+++ config_file_preparation/generate_ROI_JSON.py
@@ -0,0 +1,161 @@
+from pathlib import Path
+
+from config_file_preparation.Locator_functions import (
+    calculate_chip_offset,
+    chip_rotation_angle,
+    create_directory_with_error_handling,
+    create_ROI_JSON,
+    load_and_offset_grating_data,
+    load_chip_feature_locations,
+    load_image_and_normalise,
+    load_user_feature_locations,
+    refine_feature_locations,
+    rotate_image,
+    save_json,
+    user_chip_scale_factor,
+    visualize_features_with_matplotlib,
+)
+
+
+def generate_ROI_JSON(path_to_image, output_ROI_filename):
+    """Main function to perform image alignment and feature localization."""
+    # 1. Script input file data
+    feature_location_json = Path('Feature_locations.json')
+    chip_location_json = Path('config_file_preparation', 'Label_templates', 'Chip_map.json')
+    generated_results_root = Path('Generated_files')
+    calculated_image_feature_path = Path(generated_results_root, 'CalculatedImageFeatures.json')
+    grating_locations_image_path = Path(generated_results_root, 'Grating_locations.png')
+    label_locations_image_path = Path(generated_results_root, 'Label_locations.png')
+    template_matching_image_path = Path(generated_results_root, 'result.png')
+    output_ROI_path = Path(generated_results_root, output_ROI_filename)
+
+    result, error = create_directory_with_error_handling(generated_results_root)
+    if error:
+        print(error)
+        exit(1)
+
+    image, error = load_image_and_normalise(path_to_image)
+    if error:
+        print(error)
+        exit(1)
+
+    user_chip_mapping, error = load_user_feature_locations(feature_location_json)
+    if error:
+        print(error)
+        exit(1)
+
+    user_chip_mapping, error = load_chip_feature_locations(chip_location_json, user_chip_mapping)
+    if error:
+        print(error)
+        exit(1)
+
+    # 2. Calculate angle and scale according to user's input
+    result, error = chip_rotation_angle(user_chip_mapping, key='user_location')
+    if error:
+        print(error)
+        exit(1)
+    user_chip_mapping, initial_rotation_angle = result
+
+    result, error = user_chip_scale_factor(user_chip_mapping, key='user_location')
+    if error:
+        print(error)
+        exit(1)
+    user_chip_mapping, _ = result
+
+    # 3. Rotate image using user angle
+    rotated_image, error = rotate_image(image, -initial_rotation_angle)
+    if error:
+        print(error)
+        exit(1)
+
+    # 4. Refine feature locations using template matching
+    result, error = refine_feature_locations(rotated_image, user_chip_mapping,
+                                             template_matching_image_path)
+    if error:
+        print(error)
+        exit(1)
+    user_chip_mapping, refined_locations, _ = result
+
+    # 5. Calculate rotation-angle / scale-factor of image from locatedd the image features (not from user input locations)
+    result, error = chip_rotation_angle(user_chip_mapping, key='refined_location')
+    if error:
+        print(error)
+        exit(1)
+    user_chip_mapping, refined_rotation_angle = result
+
+    # user_chip_mapping['rotation_angle'] = initial_rotation_angle + refined_rotation_angle
+    result, error = user_chip_scale_factor(user_chip_mapping, key='refined_location')
+    if error:
+        print(error)
+        exit(1)
+    user_chip_mapping, refined_scale_factor = result
+
+    # 6. Rotate image by refined rotation angle
+    rotated_image, error = rotate_image(image, -user_chip_mapping['rotation_angle'])
+    if error:
+        print(error)
+        exit(1)
+
+    # 6. Refine feature locations again with new rotation-angle / scale-factor using template matching
+    result, error = refine_feature_locations(rotated_image, user_chip_mapping,
+                                             template_matching_image_path)
+    if error:
+        print(error)
+        exit(1)
+    user_chip_mapping, refined_locations, template_shape = result
+
+    # 7. Calculate offset between image and chip-map
+    user_chip_mapping, error = calculate_chip_offset(user_chip_mapping)
+    if error:
+        print(error)
+        exit(1)
+
+    # 8. Load and offset grating locations
+    grating_data, error = load_and_offset_grating_data(chip_location_json, user_chip_mapping)
+    if error:
+        print(error)
+        exit(1)
+
+    # 9.. Visualise feature location results
+    visualize_features_with_matplotlib(
+        rotated_image, user_chip_mapping, template_shape,
+        label_locations_image_path, key='features'
+    )
+
+    # 10. Visualise grating location results
+    visualize_features_with_matplotlib(rotated_image, grating_data, None,
+                                       grating_locations_image_path, key='gratings')
+
+    # 11. Display results
+    # pp = pprint.PrettyPrinter(indent=4)  # Create a PrettyPrinter object
+    # print('User chip mapping:')
+    # pp.pprint(user_chip_mapping)
+
+    # 12. Create ROI JSON file
+    chip_type = user_chip_mapping['chip_type']
+    rotation_angle = user_chip_mapping['rotation_angle']
+    result, error = create_ROI_JSON(
+        chip_type, grating_data, rotated_image.shape, rotation_angle, output_ROI_path
+    )
+    if error:
+        print(error)
+        exit(1)
+
+    # 13. Save calculated image features
+    result, error = save_json(calculated_image_feature_path, user_chip_mapping)
+    if error:
+        print(error)
+        exit(1)
+
+
+if __name__ == '__main__':
+    # path_to_image = Path(
+    #   '/Volumes/krauss/Lisa/GMR/Array/250325/loc1_1/Pos0/img_000000000_Default_000.tif'
+    # )
+    path_to_image = Path(
+        '/Volumes/krauss/Callum/03_Data/17_array_testing/ethanol_full_array_test_240425_flipped/image_20250424_161414.png'
+    )
+    output_ROI_filename = Path('ROI_ChirpArray_TEST.json')
+
+    generate_ROI_JSON(path_to_image, output_ROI_filename)
+
---
+++ prepare_files_for_image_analysis.py
@@ -0,0 +1,110 @@
+import shutil
+import sys
+from pathlib import Path
+
+from config_file_preparation.generate_ROI_JSON import generate_ROI_JSON
+
+###############################################################################
+## EDIT THESE VALUES ##########################################################
+
+ROOT_PATH =      '/Volumes/krauss/Callum/03_Data/17_array_testing'
+PATH_TO_IMAGES = '/Volumes/krauss/Callum/03_Data/17_array_testing/ethanol_full_array_test_240425_flipped'
+IMAGE_TYPE =     'png'
+IMAGE_NAME_FOR_ROI_PLOTS = 'image_20250424_160317'
+
+###############################################################################
+###############################################################################
+
+ROI_METADATA_NAME = 'ROI_manifest.json'
+
+
+def move_files(files_src_dest_name):
+    """
+    Copy a file from src_path to dest_path.
+
+    Args:
+        files_src_dest_name (list(tuple)): List containging tuples with src_path,
+                                           dest_path, and filename
+
+    Returns:
+        bool: True if the copy succeeded, False otherwise.
+    """
+
+    message = []
+    success = True
+    for src_path, dest_path, name in files_src_dest_name:
+        try:
+            shutil.copy(Path(src_path, name), Path(dest_path, name))
+            message.append(
+                f"Successfully copied '{Path(src_path, name)}' to '{Path(dest_path, name)}'."
+            )
+        except PermissionError:
+            message.append(
+                f"[ERROR]: Permission denied while copying '{Path(src_path, name)}' to '{Path(dest_path, name)}'."
+            )
+            success = False
+        except shutil.SameFileError:
+            message.append(
+                f"[ERROR]: Source and destination are the same file: '{Path(src_path, name)}'."
+            )
+            success = False
+        except Exception as e:
+            message.append(
+                f"[ERROR]: Failed to copy '{Path(src_path, name)}' to '{Path(dest_path, name)}': {e}"
+            )
+            success = False
+    print('\n')
+    print('\n'.join(message))
+    return success
+
+print('##############################################')
+print('Have you updated the filepaths in this script?')
+print('##############################################')
+
+# 1. Confirm root, image, ROI, and server_root variables
+print('\nStep 4: Are these locations correct?')
+print(f'\t{"Root folder:":<30} {ROOT_PATH}')
+print(f'\t{"Image folder:":<30} {PATH_TO_IMAGES}')
+print(f'\t{"Image type:":<30} {IMAGE_TYPE}')
+print(f'\t{"ROI metadata name:":<30} {ROI_METADATA_NAME}')
+input('Press Enter to continue...')
+
+# 2. Generate ROI metadata and save plots of ROI locations
+# Make sure directory exists for generated files
+Path('Generated_files').mkdir(parents=True, exist_ok=True)
+if Path('Generated_files', 'ImageFeatures.csv').exists():
+    Path('Generated_files', 'ImageFeatures.csv').unlink()
+print('\nStep 6: Generating ROI metadata...')
+generate_ROI_JSON(
+    Path(PATH_TO_IMAGES, f'{IMAGE_NAME_FOR_ROI_PLOTS}.{IMAGE_TYPE}'),
+    Path(f'{ROI_METADATA_NAME}.json'),
+)
+
+# 3. Confirm ROI locations
+print('\nStep 8: Please check the saved ROI location plots.')
+roi_correct = input('\tAre the ROI locations correct? (y/n): ').lower()
+if roi_correct != 'y':
+    print('\nROI locations not confirmed. Please adjust and rerun. Exiting.')
+    sys.exit(1)
+
+
+# 4. Back-up generated files to location of experiment
+print('\nStep 9: Backing-up all generated files to experiment directory...')
+dest_path = Path(ROOT_PATH, 'Image_analysis_files_BACKUP')
+dest_path.mkdir(parents=True, exist_ok=True)
+files_src_dest_name = []
+src_path = Path('Generated_files')
+for entry in src_path.iterdir():
+    if not entry.is_file():
+        continue
+    files_src_dest_name.append((src_path, dest_path, entry.name))
+
+src_path = Path('config')
+for entry in src_path.iterdir():
+    if not entry.is_file():
+        continue
+    files_src_dest_name.append((src_path, dest_path, entry.name))
+
+if not move_files(files_src_dest_name):
+    print('\nFailed to move some/all metadata files. Exiting.')
+    sys.exit(1)
