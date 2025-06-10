75b4b4b3c6c6b5a2cfea56d9fd71aa2030a7f792
cr806 <christopher.reardon@york.ac.uk>
2025-06-10 14:12:43 +0100

    continuous capture viewer script WIP

--- INTERNAL_FILES/continuous_capture_viewer.sh
+++ INTERNAL_FILES/continuous_capture_viewer.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-IMAGE_PATH="~sftp://phorest@192.168.1.2/home/phorest/Documents/Python/phorest_pipeline/continuous_capture/continuous_capture_frame.jpg"
+UoY='144.32.82.24'
+BIC='192.168.1.2'
+IMAGE_PATH="~sftp://phorest@$UoY/home/phorest/Documents/Python/phorest_pipeline/continuous_capture/continuous_capture_frame.jpg"
 
 feh --reload 1 --zoom fill --title "Live Image" "$IMAGE_PATH" &
 
