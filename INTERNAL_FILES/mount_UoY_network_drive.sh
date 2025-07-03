35a87a2578a9afb7d4ef6758310371f3c2278773
cr806 <christopher.reardon@york.ac.uk>
2025-07-03 10:22:56 +0100

    Mounting script now asks for username

--- INTERNAL_FILES/mount_UoY_network_drive.sh
+++ INTERNAL_FILES/mount_UoY_network_drive.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 MOUNT_POINT="/mnt/storage"
+NETWORK_ADDR="//storage.york.ac.uk/physics/krauss"
+USED_UID=$(id -u)
 
 if [ ! -d "$MOUNT_POINT" ]; then
     echo "Error: Mount point '$MOUNT_POINT' does not exist."
@@ -8,8 +10,21 @@ if [ ! -d "$MOUNT_POINT" ]; then
     exit 1
 fi
 
-echo "Mounting 'storage' to '$MOUNT_POINT'"
-sudo mount -t cifs -o username=cr806,domain=itsyork,uid=1000 //storage.york.ac.uk/physics/krauss "$MOUNT_POINT"
+read -p "Enter your University username: " user_name
+
+if [ -z "user_name" ]; then
+    echo "ERROR: No username entered. Aborting."
+    exit 1
+fi
+
+if ! [[ "$user_name" =~ [[:alpha:]] && "$user_name" =~ [[:digit:]] ]]; then
+    echo "Error: Badly formed username. It must contain both letters and numbers."
+    exit 1
+fi
+
+echo "Attempting to mount '$NETWORK_ADDR' to '$MOUNT_POINT' for user '$user_name'..."
+
+sudo mount -t cifs -o username="$user_name",domain=itsyork,uid=$USED_UID $NETWORK_ADDR "$MOUNT_POINT"
 
 # Check if the mount was successful
 if [ $? -eq 0 ]; then
