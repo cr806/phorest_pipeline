c6d6b91de8b67176b035a6293f6b1294bf5a4b0b
cr806 <christopher.reardon@york.ac.uk>
2025-08-07 11:07:57 +0100

    Create function to safely create directory but also clear directory of old files

--- src/phorest_pipeline/analysis/file_utils.py
+++ src/phorest_pipeline/analysis/file_utils.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import shutil
+
 
 def create_directory_with_error_handling(directory_path: Path):
     message = "at function create_directory_with_error_handling"
@@ -8,6 +10,39 @@ def create_directory_with_error_handling(directory_path: Path):
     except FileExistsError:
         # This should be rare with exist_ok=True, but handle just in case
         return (None, f"[WARNING] Directory '{directory_path}' already exists {message}.")
+    except FileNotFoundError as e:
+        return (
+            None,
+            f"[ERROR] Could not create directory '{directory_path}' - likely a permission issue) {message}: {e}",
+        )
+    except PermissionError as e:
+        return (
+            None,
+            f"[ERROR] Permission denied to create directory '{directory_path}' {message}: {e}",
+        )
+    except OSError as e:
+        return (
+            None,
+            f"[ERROR] Operating system error occurred while creating directory '{directory_path}' {message}: {e}",
+        )
+    except Exception as e:
+        return (
+            None,
+            f"[ERROR] An unexpected error occurred while creating directory '{directory_path}' {message}: {e}",
+        )
+    
+def clear_and_create_directory_with_error_handling(directory_path: Path):
+    message = "at function create_directory_with_error_handling"
+    try:
+        if directory_path.is_dir():
+            for item in directory_path.iterdir():
+                if item.is_dir():
+                    shutil.rmtree(item)
+                else:
+                    item.unlink()
+
+        directory_path.mkdir(parents=True, exist_ok=True)
+        return (None, None)
     except FileNotFoundError as e:
         return (
             None,
